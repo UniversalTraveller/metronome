a4abecf5f93effa106767d9567a22fb451365566
Andreas Kiener <andreas.kiener@gmail.com>
2026-08-15 13:20:37 +0200

    Implement BPM control with increment and decrement buttons

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,19 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  const [bpm, setBpm] = useState<number>(120);
+
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Pressable onPress={() => setBpm((prev) => prev - 1)}>
+        <Text style={styles.button}>-</Text>
+      </Pressable>
+
+      <Text style={styles.bpmDisplay}>{bpm}</Text>
+      <Pressable onPress={() => setBpm((prev) => prev + 1)}>
+        <Text style={styles.button}>+</Text>
+      </Pressable>
     </View>
   );
 }
@@ -11,7 +21,16 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
+  },
+  bpmDisplay: {
+    fontSize: 48,
+    marginHorizontal: 20,
+  },
+  button: {
+    fontSize: 24,
+    marginHorizontal: 10,
   },
 });
